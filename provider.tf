106b5b6d7085136823cd86b6d803934deb35f7b4
Dapo <106677980+Dapo-code@users.noreply.github.com>
2024-12-02 22:28:06 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -20,6 +20,6 @@ provider "azurerm" {
     subscription_id = "5d519402-d62b-4a0d-93cd-bd98fe938726"
     tenant_id = "536c4d46-b757-419d-abbd-a28c79ef3064"
     client_id = "c7694489-75ee-4db5-ac89-d38cc1b6deb2"
-    client_secret = "dyX8Q~J5qzj-.fdd2UfF8j-uTPQiHic5IgAu6bnY"
+    client_secret = ""
     features {}
 }
